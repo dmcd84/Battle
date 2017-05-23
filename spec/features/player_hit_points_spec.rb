bc889819a1c961d38f782623a28307f5ff9efaf7
IAN SIMPSON <ianconradsimpson@gmail.com>
2017-05-23 17:53:47 +0100

    Attack button added

--- spec/features/player_hit_points_spec.rb
+++ spec/features/player_hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'request player hit points' do
   scenario 'see player hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Cuddles'
-    fill_in :player_2_name, with: 'Brian'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Brian: 100HP'
   end
 end
