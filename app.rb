4300c9024fe6cfbc9735a6386599fc899e3af244
Dom <dom.mcdonnell@gmail.com>
2017-05-23 16:52:02 +0100

    update app.rb to use sessions for player names.

--- app.rb
+++ app.rb
@@ -1,5 +1,4 @@
 require 'sinatra/base'
-
 class Battle < Sinatra::Base
   # get '/' do
   #   'Testing infrastructure working!'
@@ -7,13 +6,21 @@ class Battle < Sinatra::Base
 
   run! if app_file == $0
 
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 end
